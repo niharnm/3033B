08ca5352870d19ff4bd93e8d86a6062d86a73a46
Nihar <117209695+niharnm@users.noreply.github.com>
2025-03-18 18:41:52 -0700

    Update robot-config.h

--- bruh/3303BACTUALCODE/include/robot-config.h
+++ bruh/3303BACTUALCODE/include/robot-config.h
@@ -1,29 +1,20 @@
 using namespace vex;
 
 extern brain Brain;
-extern motor Intake;
-extern motor LadyBrown ;
 
-extern digital_out Clamp ;
-extern digital_out Doinker;
-extern digital_out IntakeLevel;
 //To set up a motor called LeftFront here, you'd use
 //extern motor LeftFront;
 
 //Add your devices below, and don't forget to do the same in robot-config.cpp:
+extern motor intake1;
+extern controller Controller1;
+extern digital_out mogoMount;
+extern inertial DrivetrainInertial;
+extern motor ladybrown;
+extern rotation armrot;
+extern digital_out Clamp;
+extern digital_out Flag;
 
-
-void Raise(void);
-void Score(void);
-void Release(void);
-void ClampIn(void);
-void ClampOut(void);
-void DoinkerIn(void);
-void DoinkerOut(void);
-void IntakeLiftUp(void);
-void IntakeLiftDown(void);
-void vexcodeInit( void );
-void toggleDoinker(void);
-void toggleClamp(void);
-void opcontrol();
-void usercontrol();
+void LadyBrownMove (void);
+void LadyBrownUp (void);
+void  vexcodeInit( void );
